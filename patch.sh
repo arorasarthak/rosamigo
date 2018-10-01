815796a33aced0b1e872cbf29de23616ade3ff3e
arorasarthak <sarthak.arora@live.com>
2018-09-30 23:03:11 -0400

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Setting up permissions..."
-chmod +x $HOME/amigo_ws/src/rosaria/cfg/RosAria.cfg
-chmod +x $HOME/amigo_ws/src/mypkg/my_publisher.py
-chmod +x $HOME/amigo_ws/src/mypkg/my_subscriber.py
+chmod +x $HOME/$YOUR_WS/src/rosaria/cfg/RosAria.cfg
+chmod +x $HOME/$YOUR_WS/src/mypkg/my_publisher.py
+chmod +x $HOME/$YOUR_WS/src/mypkg/my_subscriber.py
 echo "Setup complete.."

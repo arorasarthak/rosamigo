a518179ef2316cb71f915a7b1716ac8c528b5707
arorasarthak <sarthak.arora@live.com>
2018-09-30 22:37:40 -0400

    Add files via upload
    Fix for permissions for RosAria.cfg, my_publisher.py, my_subscriber.py

---
+++ patch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Setting up permissions..."
+chmod +x $HOME/amigo_ws/src/rosaria/cfg/RosAria.cfg
+chmod +x $HOME/amigo_ws/src/mypkg/my_publisher.py
+chmod +x $HOME/amigo_ws/src/mypkg/my_subscriber.py
+echo "Setup complete.."
